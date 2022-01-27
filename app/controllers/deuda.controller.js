daa2ec8789afd8bb8a1fa4d79df77ef202ed893c
jaglobal <proyectosjagloblal@gmail.com>
2022-01-27 19:19:12 -0400

    comentarios agregados

--- app/controllers/deuda.controller.js
+++ app/controllers/deuda.controller.js
@@ -1,17 +1,18 @@
 const db = require("../models");
 const Deuda = db.deudas;
 
-// Create and Save a new deuda
+// Crear y almacenar una deuda en la DB
 exports.create = async (req, res) => {
 
-  return res.send(req.body)
-  // Se guarda en la variable "deuda" el objeto que se almacenara en la DB.
+  // Se preparan los datos provenientes del request
+  // con los campo de la DB que les corresponde:
   const nuevaDeuda = new Deuda({
     descripcion: req.body.descripcion,
     monto: req.body.monto
   })
 
-  await nuevaDeuda.save()
+  await nuevaDeuda.save() // Se ejecuta el guardado del registro nuevo
 
-  return res.send('deuda almacenada con exito')
+  res.json({message: 'deuda almacenada con exito'}) // Mensaje retornado al frontend o cliente.
+  
 }
--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+
+// -------------------------------------- PREPARATIVOS ------------------------------------------
+
 const express = require("express")
 const cors = require("cors");
 require("./app/config/initial.config")
@@ -10,37 +13,57 @@ app.use(express.json())
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }))
 
-// ---------------------------------------- INICIO---------------------------------------------
+// ---------------------------------------- EJERCICIO #1 ---------------------------------------------
 
-function generarMensaje (autor) {
-  console.log('Ejecutando generarMensaje() ...');
-  let mensaje = "Welcome to "+ autor + " first CRUD."
-  return mensaje
-}
-
-// simple route
+// ------------- Ruta simple por GET accesible desde el navegador:
 app.get("/", (req, res) => {
+
   let mensaje = generarMensaje("kathe's") // cableado
-  res.json({ message:  mensaje})
+
+  res.json({ message:  mensaje}) 
+  // res = respuesta al cliente o frontend
+  // {"message":"Welcome to kathe's first CRUD."}
 })
 
+// ----------- Ruta por POST accesible desde frontend o cliente (Postman):
 app.post("/dinamico", (req, res) => {
-  console.log('req.body :>> ', req.body)
+
+  console.log('req.body :>> ', req.body) // req = datos de la peticion
+
   let autorRequest = req.body.autor
   let mensaje = generarMensaje(autorRequest) // dinamico desde el request
   res.json({ message:  mensaje})
+
 })
 
+// ----------- Ruta por POST accesible desde frontend o cliente (Postman):
 app.post("/abstracto", mensajeAbstracto) // funcion ubicada en cualquier otro archivo
 
+
+// ----------- Funciones reutilizables del Ejercicio #1:
+
 function mensajeAbstracto (req, res) { // recibe el req y res como parametros
+
   console.log('req.body :>> ', req.body)
+  
   let mensaje = generarMensaje(req.body.autor)
   res.json({ message:  mensaje})
+
 }
 
-// const deudas = require("./app/controllers/deuda.controller.js")
-// app.post("/creardeuda", deudas.create)
+function generarMensaje (autor) {
+
+  console.log('Ejecutando generarMensaje() ...');
+
+  let mensaje = "Welcome to "+ autor + " first CRUD."
+  return mensaje
+
+}
+
+
+// ---------------------------------------- EJERCICIO #2 ---------------------------------------------
+const deudas = require("./app/controllers/deuda.controller.js")
+app.post("/creardeuda", deudas.create)
 
 // require("./app/routes/deuda.routes")(app);
 
